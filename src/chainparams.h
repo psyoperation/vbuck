aeeb737c2e764ec8437621c2f98e8038ac357a60
MrWhiteHand <blanchemain.Florian@gmail.com>
2018-07-16 22:55:53 +0200

    Generator seeds update

--- src/chainparams.h
+++ src/chainparams.h
@@ -6,8 +6,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_CHAINPARAMS_H
-#define BITCOIN_CHAINPARAMS_H
+#ifndef CHAINPARAMS_H
+#define CHAINPARAMS_H
 
 #include "chainparamsbase.h"
 #include "checkpoints.h"
